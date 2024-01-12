95dfd29992ebf6b919f65ca5b5785df88f1c071b
Tomasz Myśliwiec <tomasz.mysliwiec@execon.pl>
2024-01-12 11:05:32 +0100

    chore: add linting an husky

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
 import { parse } from 'rss-to-json';
 import cors from 'cors';
-import dataSources from "./utils/data-sources";
-
+import dataSources from './utils/data-sources';
 
 type FeedItem = {
   title: string;
@@ -31,23 +30,27 @@ app.use(cors());
 
 app.get('/feed', async (req: Request, res: Response) => {
   const feedArray: FeedArray[] = [];
- 
-  await Promise.all(dataSources.map(async (mainItem, mainIndex) => {
-    try {
-      await Promise.all(mainItem.sources.map(async (source, sourceIndex) => {
-        if (source.active) {
-          const feed = await parse(source.url);
-          feedArray.push({
-            ...feed,
-            mainIdx: mainIndex,
-            sourceIdx: sourceIndex,
-          });
-        } 
-      }));
-    } catch (error) {
-      console.error(error);
-    }
-  }));
+
+  await Promise.all(
+    dataSources.map(async (mainItem, mainIndex) => {
+      try {
+        await Promise.all(
+          mainItem.sources.map(async (source, sourceIndex) => {
+            if (source.active) {
+              const feed = await parse(source.url);
+              feedArray.push({
+                ...feed,
+                mainIdx: mainIndex,
+                sourceIdx: sourceIndex,
+              });
+            }
+          }),
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    }),
+  );
 
   const responseData = dataSources.map((mainItem, mainIndex) => {
     return {
@@ -55,15 +58,15 @@ app.get('/feed', async (req: Request, res: Response) => {
       sources: mainItem.sources.map((source, sourceIndex) => {
         return {
           ...source,
-          feed: feedArray.find((item) => item.mainIdx === mainIndex && item.sourceIdx === sourceIndex)?.items
-        }
-      })
-    }
+          feed: feedArray.find((item) => item.mainIdx === mainIndex && item.sourceIdx === sourceIndex)?.items,
+        };
+      }),
+    };
   });
 
   res.send(responseData);
-})
+});
 
 app.listen(port, () => {
-  console.log(`Read Point Backend services listening on port ${port}`)
-})
+  console.log(`Read Point Backend services listening on port ${port}`);
+});
